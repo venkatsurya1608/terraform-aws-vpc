b627ce2fa020eaf69dc1e2bfc7fddc1575d2df7f
venkatsurya <venkatsurya1608@gmail.com>
2024-05-30 14:57:14 +0530

    some-message

--- outputs.tf
+++ outputs.tf
@@ -22,9 +22,9 @@ output "database_subnet_group_id" {
   value = aws_db_subnet_group.default.id
 }
 
-# output "database_subnet_group_name" {
-#   value = aws_db_subnet_group.default.name
-# }
+output "database_subnet_group_name" {
+  value = aws_db_subnet_group.default.name
+}
 
 output "igw_id" {
   value = aws_internet_gateway.gw.id
